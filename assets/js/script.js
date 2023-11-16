512c648092e55e6e02c0207b1b19d690d8323332
buiduytrung2000 <buiduytrung2015@gmail.com>
2023-11-16 12:05:28 +0700

    fix js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1195,28 +1195,28 @@ function _0x509e() {
           : _0x13520a["attr"]("type", _0x1f4d42(0xff));
       });
   })(window["jQuery"]);
-document.ready(function () {
-  $("a[href*=#]").bind("click", function (e) {
-    e.preventDefault(); // prevent hard jump, the default behavior
+// document.ready(function () {
+//   $("a[href*=#]").bind("click", function (e) {
+//     e.preventDefault(); // prevent hard jump, the default behavior
 
-    var target = $(this).attr("href"); // Set the target as variable
+//     var target = $(this).attr("href"); // Set the target as variable
 
-    // perform animated scrolling by getting top-position of target-element and set it as scroll target
-    $("html, body")
-      .stop()
-      .animate(
-        {
-          scrollTop: $(target).offset().top,
-        },
-        600,
-        function () {
-          location.hash = target; //attach the hash (#jumptarget) to the pageurl
-        }
-      );
+//     // perform animated scrolling by getting top-position of target-element and set it as scroll target
+//     $("html, body")
+//       .stop()
+//       .animate(
+//         {
+//           scrollTop: $(target).offset().top,
+//         },
+//         600,
+//         function () {
+//           location.hash = target; //attach the hash (#jumptarget) to the pageurl
+//         }
+//       );
 
-    return false;
-  });
-});
+//     return false;
+//   });
+// });
 
 $(window)
   .scroll(function () {
